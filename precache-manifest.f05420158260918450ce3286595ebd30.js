0306bb88cd187c884e8f8d32b3af07c91811ded6
Tumelom2 <tj.malumane@gmail.com>
2020-01-06 12:08:36 +0200

    Updates

---
+++ precache-manifest.f05420158260918450ce3286595ebd30.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4e1b89657cdf27233206191dcde4385",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "28b1003f8ec929f5c5c6",
+    "url": "/monsters-rolodex/static/css/main.4a4ea99f.chunk.css"
+  },
+  {
+    "revision": "46012db1d69cedf8d1ab",
+    "url": "/monsters-rolodex/static/js/2.3d8af1b9.chunk.js"
+  },
+  {
+    "revision": "28b1003f8ec929f5c5c6",
+    "url": "/monsters-rolodex/static/js/main.7aed5b14.chunk.js"
+  },
+  {
+    "revision": "5f535753dcec63ef3476",
+    "url": "/monsters-rolodex/static/js/runtime~main.7352cc78.js"
+  }
+]);
